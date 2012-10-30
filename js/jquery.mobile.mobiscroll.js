f03ef35eeb8d6ae3f91b01d5de461e8b3b25326c
Istvan Halmen <dioslaska@yahoo.com>
2012-10-30 16:44:45 +0200

    jQuery Mobile widget code cleanup

--- js/jquery.mobile.mobiscroll.js
+++ js/jquery.mobile.mobiscroll.js
@@ -15,45 +15,35 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 (function($, undefined ) {
-$.widget( "mobile.datebox", $.mobile.widget, {
-    options: {
-        theme: 'jqm',
-        lang: 'en',
-        mode: 'scroller',
-        disableManualInput: false,
-        preset: 'date'
-    },
-    _create: function(){
-        var self = this;
-        var o = $.extend(this.options, this.element.data('options'));
-        var input = this.element;
-        $(input).scroller(o);
-        $(input).on('click', function() {
-            $(this).scroller('show');
-        });
-        
-        if (o.disableManualInput) $(input).attr("readonly", true);
-    },
-});
-$(document).bind(
-        "pagebeforecreate",
-        function(c) {
-            $(":jqmData(role='datebox')", c.target).each(
-                    function() {
-                        $(this).replaceWith(
-                                $("<div>").html($(this).clone()).html()
-                                        .replace(/\s+type=["']date['"]?/,
-                                                ' type="text" '))
-                    })
-        });
-$(document).bind("pagecreate create", function(c) {
-    $(document).trigger("dateboxbeforecreate");
-    $(":jqmData(role='datebox')", c.target).each(function() {
-        if (typeof ($(this).data("datebox")) === "undefined") {
-            $(this).datebox();
+
+    $.widget( "mobile.datebox", $.mobile.widget, {
+        options: {
+            theme: 'jqm',
+            preset: 'date'
+        },
+        _create: function() {
+            var o = $.extend(this.options, this.element.data('options')),
+                input = $(this.element);
+            input.scroller(o);
         }
-    })
-})
+    });
+
+    $(document).bind("pagebeforecreate", function(c) {
+        // Convert date inputs to normal inputs
+        $('input[type="date"]:jqmData(role="datebox")', c.target).prop('type', 'text');
+    });
+
+    $(document).bind("pagecreate create", function(c) {
+        $(document).trigger("dateboxbeforecreate");
+
+        $(':jqmData(role="datebox")', c.target).each(function() {
+            if (typeof ($(this).data("datebox")) === "undefined") {
+                $(this).datebox();
+            }
+        })
+    });
+
 })( jQuery );
 
