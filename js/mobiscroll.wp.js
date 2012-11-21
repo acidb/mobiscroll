c1ca21cf2695416fc8aa7916fb402272709171b7
Istvan Halmen <dioslaska@yahoo.com>
2012-11-21 16:38:12 +0200

    Add Windows Phone theme

---
+++ js/mobiscroll.wp.js
@@ -0,0 +1,32 @@
+(function ($) {
+
+    $.mobiscroll.themes.wp = {
+        defaults: {
+            width: 70,
+            height: 76,
+            dateOrder: 'mmMMddDDyy'
+        },
+        init: function(elm, inst) {
+            var click,
+                active;
+
+            $('.dwwl', elm).bind('touchstart mousedown DOMMouseScroll mousewheel', function() {
+                click = true;
+                active = $(this).hasClass('wpa');
+                $('.dwwl', elm).removeClass('wpa');
+                $(this).addClass('wpa');
+            }).bind('touchmove mousemove', function() {
+                click = false;
+            }).bind('touchend mouseup', function() {
+                if (click && active) {
+                    $(this).removeClass('wpa');
+                }
+            });
+        }
+    }
+
+    $.mobiscroll.themes['wp light'] = $.mobiscroll.themes.wp;
+
+})(jQuery);
+
+
