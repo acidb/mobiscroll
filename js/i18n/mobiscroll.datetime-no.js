d7c55644de0fe7f4c087d82164a8f33dc8327c3c
Tor Hovland <tor@nidaros.homedns.org>
2012-12-27 00:47:35 -0800

    Create mobiscroll.datetime-no.js

---
+++ js/i18n/mobiscroll.datetime-no.js
@@ -0,0 +1,19 @@
+(function ($) {
+    $.mobiscroll.i18n.no = $.extend($.mobiscroll.i18n.no, {
+        dateFormat: 'dd.mm.yy',
+        dateOrder: 'ddmmyy',
+        dayNames: ['Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag'],
+        dayNamesShort: ['Sø', 'Ma', 'Ti', 'On', 'To', 'Fr', 'Lø'],
+        dayText: 'Dag',
+        hourText: 'Time',
+        minuteText: 'Minutt',
+        monthNames: ['Januar', 'Februar', 'Mars', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Desember'],
+        monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'],
+        monthText: 'Måned',
+        secText: 'Sekund',
+        timeFormat: 'HH:ii',
+        timeWheels: 'HHii',
+        yearText: 'År',
+        nowText: 'Nå'
+    });
+})(jQuery);
