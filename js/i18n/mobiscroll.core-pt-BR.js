5b263ecabee1502fe33d614c6a063d0699ea773a
Istvan Halmen <dioslaska@yahoo.com>
2013-01-03 12:07:20 +0200

    Brazilian Portuguese translation

---
+++ js/i18n/mobiscroll.core-pt-BR.js
@@ -0,0 +1,10 @@
+/*
+ * Translation by: Ivan Gomes <contato@ivangomes.com.br>
+ *
+ */
+(function ($) {
+    $.mobiscroll.i18n['pt-BR'] = $.extend($.mobiscroll.i18n['pt-BR'], {
+        setText: 'Selecionar',
+        cancelText: 'Cancelar'
+    });
+})(jQuery);
---
+++ js/i18n/mobiscroll.datetime-pt-BR.js
@@ -0,0 +1,22 @@
+/*
+ * Translation by: Ivan Gomes <contato@ivangomes.com.br>
+ *
+ */
+(function ($) {
+    $.mobiscroll.i18n['pt-BR'] = $.extend($.mobiscroll.i18n['pt-BR'], {
+        dateFormat: 'dd/mm/yy',
+        dateOrder: 'ddMMyy',
+        dayNames: ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sábado'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb'],
+        dayText: 'Dia',
+        hourText: 'Hora',
+        minuteText: 'Minutos',
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+        monthText: 'Mês',
+        secText: 'Segundo',
+        timeFormat: 'HH:ii',
+        timeWheels: 'HHii',
+        yearText: 'Ano'
+    });
+})(jQuery);
