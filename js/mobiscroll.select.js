c2b0cee484e40a921e23ebad221f033aa6f50ef8
Istvan Halmen <dioslaska@yahoo.com>
2013-03-12 12:13:01 +0200

    Select preset indentation fix

--- js/mobiscroll.select.js
+++ js/mobiscroll.select.js
@@ -186,8 +186,7 @@
                             prev = gr + '';
                             return false;
                         }
-                    }
-                    else {
+                    } else {
                         stg.readonly = roPre;
                     }
                 } else {
