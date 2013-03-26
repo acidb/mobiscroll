15bbe3c752fa7242c93493edb98dbff81f6432c2
Istvan Halmen <dioslaska@yahoo.com>
2013-03-26 11:47:15 +0200

    jqm theme clickpick fixes

--- js/mobiscroll.jqm.js
+++ js/mobiscroll.jqm.js
@@ -19,7 +19,8 @@
             $('.dwwb', elm).attr('data-role', 'button').attr('data-theme', s.jqmClickPick);
             $('.dwv', elm).addClass('ui-header ui-bar-' + s.jqmHeader);
             $('.dwwr', elm).addClass('ui-body-' + s.jqmBody);
-            $('.dwpm .dww', elm).addClass('ui-body-' + s.jqmWheel);
+            $('.dwpm .dwwl', elm).addClass('ui-body-' + s.jqmWheel);
+            $('.dwpm .dwl', elm).addClass('ui-body-' + s.jqmBody);
             elm.trigger('create');
             // Hide on overlay click
             $('.dwo', elm).click(function () { inst.cancel(); });
