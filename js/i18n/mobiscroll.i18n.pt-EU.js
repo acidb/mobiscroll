c36bdfd9563cd63fd0dac6c52d13c5ebd55e0e51
Jorge Simões <jorgefsimoes@users.noreply.github.com>
2013-08-26 13:18:05 +0100

    Create mobiscroll.i18n.pt-EU.js
    Translation to European Portuguese

---
+++ js/i18n/mobiscroll.i18n.pt-EU.js
@@ -0,0 +1,46 @@
+// Translation to pt-EU by: Jorge Simoes <http://jorge.simoes.com>
+
+(function ($) {
+    $.mobiscroll.i18n['pt-EU'] = $.extend($.mobiscroll.i18n['pt-EU'], {
+        // Core
+        setText: 'Seleccionar',
+        cancelText: 'Cancelar',
+        // Datetime component
+        dateFormat: 'dd-mm-yy',
+        dateOrder: 'ddMMyy',
+        dayNames: ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','S&aacute;bado'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','S&aacute;b'],
+        dayText: 'Dia',
+        hourText: 'Horas',
+        minuteText: 'Minutos',
+        monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
+        monthText: 'M&ecirc;s',
+        secText: 'Segundo',
+        timeFormat: 'HH:ii',
+        timeWheels: 'HHii',
+        yearText: 'Ano',
+        nowText: 'Actualizar',
+        // Calendar component
+        dateText: 'Data',
+        timeText: 'Tempo',
+        calendarText: 'Calend&aacute;rio',
+        closeText: 'Fechar',
+        // Daterange component
+        fromText: 'In&iacute;cio',
+        toText: 'Fim',
+        // Measurement components
+        wholeText: 'Inteiro',
+        fractionText: 'Frac&ccedil;&atilde;o',
+        unitText: 'Unidade',
+        // Time / Timespan component
+        labels: ['Anos', 'Meses', 'Dias', 'Horas', 'Minutos', 'Segundos', ''],
+        labelsShort: ['Ano', 'M&ecirc;s', 'Dia', 'Hora', 'Min', 'Seg', ''],
+        // Timer component
+        startText: 'Come&ccedil;ar',
+        stopText: 'Parar',
+        resetText: 'Reinicializar',
+        lapText: 'Lap',
+        hideText: 'Esconder'
+    });
+})(jQuery);
