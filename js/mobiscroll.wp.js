0faf22fe45484679a97f02cd2043a1965a93f9de
Istvan Halmen <dioslaska@yahoo.com>
2013-09-10 16:31:17 +0300

    WP Theme dateOrder fix (#153)

--- js/mobiscroll.wp.js
+++ js/mobiscroll.wp.js
@@ -19,8 +19,8 @@
             }
         },
         load: function (lang, s) {
-            var ord = lang.dateOrder;
-            if (ord && !s.dateOrder) {
+            if (lang && lang.dateOrder && !s.dateOrder) {
+                var ord = lang.dateOrder;
                 ord = ord.match(/mm/i) ? ord.replace(/mmMM|mm|MM/,  'mmMM') : ord.replace(/mM|m|M/,  'mM');
                 ord = ord.match(/dd/i) ? ord.replace(/ddDD|dd|DD/,  'ddDD') : ord.replace(/dD|d|D/,  'dD');
                 s.dateOrder = ord;
