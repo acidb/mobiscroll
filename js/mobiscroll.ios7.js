5d12161f40d4108ed5aaa0eac84178a9d9ae849f
Istvan Halmen <dioslaska@yahoo.com>
2013-09-10 14:44:18 +0300

    Added ios7 theme files

---
+++ js/mobiscroll.ios7.js
@@ -0,0 +1,16 @@
+(function ($) {
+
+    $.mobiscroll.themes.ios7 = {
+        defaults: {
+            dateOrder: 'MMdyy',
+            rows: 5,
+            height: 34,
+            width: 55,
+            headerText: false,
+            showLabel: false,
+            btnWidth: false,
+            useShortLabels: true
+        }
+    };
+
+})(jQuery);
