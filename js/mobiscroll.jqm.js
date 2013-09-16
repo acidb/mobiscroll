2e0d71060ea7e6a4a91a990e25086609a09039cb
szily20 <szily20@gmail.com>
2013-09-16 13:51:25 +0300

    jqm.js clear button added

--- js/mobiscroll.jqm.js
+++ js/mobiscroll.jqm.js
@@ -13,9 +13,8 @@
         init: function (elm, inst) {
             var s = inst.settings;
             $('.dw', elm).removeClass('dwbg').addClass('ui-overlay-shadow ui-corner-all ui-body-' + s.jqmBorder);
-            $('.dwb-s .dwb', elm).attr('data-role', 'button').attr('data-mini', 'true').attr('data-theme', s.jqmSet);
-            $('.dwb-n .dwb', elm).attr('data-role', 'button').attr('data-mini', 'true').attr('data-theme', s.jqmCancel);
-            $('.dwb-c .dwb', elm).attr('data-role', 'button').attr('data-mini', 'true').attr('data-theme', s.jqmCancel);
+            $('.dwbc .dwb',elm).attr('data-role', 'button').attr('data-mini', 'true').attr('data-theme', s.jqmCancel);
+            $('.dwb-s .dwb', elm).attr('data-theme', s.jqmSet);
             $('.dwwb', elm).attr('data-role', 'button').attr('data-theme', s.jqmClickPick);
             $('.dwv', elm).addClass('ui-header ui-bar-' + s.jqmHeader);
             $('.dwwr', elm).addClass('ui-body-' + s.jqmBody);
