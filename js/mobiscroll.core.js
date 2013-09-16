bbd2e738409d132c7714acd742c55b2191dcad1a
Istvan Halmen <dioslaska@yahoo.com>
2013-09-16 15:25:28 +0300

    Fix for tap and Android blue highlight

--- js/mobiscroll.core.js
+++ js/mobiscroll.core.js
@@ -680,10 +680,7 @@
                     if (Math.abs(getCoord(e, 'X') - startX) < 20 && Math.abs(getCoord(e, 'Y') - startY) < 20) {
                         handler.call(this, e);
                     }
-                    tap = true;
-                    setTimeout(function () {
-                        tap = false;
-                    }, 300);
+                    setTap();
                 });
             }
 
@@ -843,7 +840,9 @@
                 .on('keydown', '.dwwl', onKeyDown)
                 .on('keyup', '.dwwl', onKeyUp)
                 .on(START_EVENT, '.dwb-e', onBtnStart)
+                .on('mousedown', function (e) { e.preventDefault(); }) // Prevents blue highlight on Android
                 .on('click', '.dwb-e', function (e) { e.preventDefault(); })
+                .on('touchend', setTap)
                 .on('keydown', '.dwb-e', function (e) {
                     if (e.keyCode == 32) { // Space
                         e.preventDefault();
@@ -930,7 +929,7 @@
             elmList.push(elm);
             if (s.display !== 'inline') {
                 elm.on((s.showOnFocus ? 'focus.dw' : '') + (s.showOnTap ? ' click.dw' : ''), function () {
-                    if (!preventShow) {
+                    if (!preventShow && !tap) {
                         if (beforeShow) {
                             beforeShow();
                         }
@@ -987,7 +986,7 @@
             buttons = [];
             wndw = $(s.context == 'body' ? window : s.context);
             doc = $(s.context == 'body' ? document : wndw);
-            
+
             that.context = wndw;
             that.live = !modal || !s.setText;
 
@@ -1143,6 +1142,13 @@
             return false;
         }
         return true;
+    }
+
+    function setTap() {
+        tap = true;
+        setTimeout(function () {
+            tap = false;
+        }, 300);
     }
 
     function getCoord(e, c) {
