9d9582855caea2eb703df2a638d02dbb7c3ab366
Vladimir Gorej <gorej@codescale.net>
2014-02-06 18:52:22 +0100

    Czech language support.

---
+++ js/i18n/mobiscroll.i18n.cs.js
@@ -0,0 +1,48 @@
+(function ($) {
+    $.mobiscroll.i18n['cs-CZ'] = $.extend($.mobiscroll.i18n['cs-CZ'], {
+        // Core
+        setText: 'Zadej',
+        cancelText: 'Storno',
+        clearText: 'Vymazat',
+        selectedText: 'Označený',
+        // Datetime component
+        dateFormat: 'dd/mm/yy',
+        dateOrder: 'ddmmy',
+        dayNames: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'],
+        dayNamesShort: ['Ne', 'Po', 'Ut', 'St', 'Čt', 'Pá', 'So'],
+        dayText: 'Den',
+        hourText: 'Hodiny',
+        minuteText: 'Minuty',
+        monthNames: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+        monthNamesShort: ['Led', 'Úno', 'Bře', 'Dub', 'Kvě', 'Čer', 'Čvc', 'Spr', 'Zář', 'Říj', 'Lis', 'Pro'],
+        monthText: 'Měsíc',
+        secText: 'Sekundy',
+        timeFormat: 'hh:ii A',
+        timeWheels: 'hhiiA',
+        yearText: 'Rok',
+        nowText: 'Teď',
+        amText: 'am',
+        pmText: 'pm',
+        // Calendar component
+        dateText: 'Datum',
+        timeText: 'Čas',
+        calendarText: 'Kalendář',
+        closeText: 'Zavřít',
+        // Daterange component
+        fromText: 'Začátek',
+        toText: 'Konec',
+        // Measurement components
+        wholeText: 'Celý',
+        fractionText: 'Část',
+        unitText: 'Jednotka',
+        // Time / Timespan component
+        labels: ['Roky', 'Měsíce', 'Dny', 'Hodiny', 'Minuty', 'Sekundy', ''],
+        labelsShort: ['Rok', 'Měs', 'Dny', 'Hod', 'Min', 'Sec', ''],
+        // Timer component
+        startText: 'Start',
+        stopText: 'Stop',
+        resetText: 'Resetovat',
+        lapText: 'Etapa',
+        hideText: 'Schovat'
+    });
+})(jQuery);
