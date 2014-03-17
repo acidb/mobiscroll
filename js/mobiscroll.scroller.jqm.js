e528ef87ba80b160f3da35f5400736f8e109f12b
Istvan Halmen <dioslaska@gmail.com>
2014-03-17 10:48:39 +0200

    Theme fixes

--- js/mobiscroll.scroller.jqm.js
+++ js/mobiscroll.scroller.jqm.js
@@ -34,7 +34,7 @@
             $('.dwb-s .dwb', elm).attr('data-theme', s.jqmSet);
             $('.dwwb', elm).attr('data-role', 'button').attr('data-theme', s.jqmClickPick);
             $('.dwv', elm).addClass('ui-header ui-bar-' + s.jqmHeader);
-            $('.dwwr', elm).addClass('ui-body-' + s.jqmBody);
+            $('.dwwr', elm).addClass('ui-corner-all ui-body-' + s.jqmBody);
             $('.dwpm .dwwl', elm).addClass('ui-body-' + s.jqmWheel);
             $('.dwpm .dwl', elm).addClass('ui-body-' + s.jqmBody);
             // Calendar base
