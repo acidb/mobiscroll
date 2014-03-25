eb18a3f254e59ae7791bf9f089bef895626b9d54
Istvan Halmen <dioslaska@gmail.com>
2014-03-25 10:14:10 +0200

    Add font icons to WP theme

--- js/mobiscroll.scroller.wp.js
+++ js/mobiscroll.scroller.wp.js
@@ -15,6 +15,13 @@
 
             $('.dw', elm).addClass('wp-' + inst.settings.accent);
 
+            $('.dwb-s .dwb', elm).addClass('mbsc-ic mbsc-ic-checkmark');
+            $('.dwb-c .dwb', elm).addClass('mbsc-ic mbsc-ic-close');
+            $('.dwb-cl .dwb', elm).addClass('mbsc-ic mbsc-ic-close');
+            $('.dwb-n .dwb', elm).addClass('mbsc-ic mbsc-ic-loop2');
+            $('.dwwbp', elm).addClass('mbsc-ic mbsc-ic-plus');
+            $('.dwwbm', elm).addClass('mbsc-ic mbsc-ic-minus');
+
             $('.dwwl', elm).on('touchstart mousedown DOMMouseScroll mousewheel', function (e) {
                 if (e.type === 'mousedown' && touch) {
                     return;
