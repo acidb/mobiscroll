f1017ed2527fce7df2947353f0d771ca36819d2d
szily20 <szily20@gmail.com>
2014-04-18 08:52:44 +0300

    timer buttons use fonts

--- js/mobiscroll.scroller.android.js
+++ js/mobiscroll.scroller.android.js
@@ -4,7 +4,11 @@
         dateOrder: 'Mddyy',
         mode: 'clickpick',
         height: 50,
-        showLabel: false
+        showLabel: false,
+        btnStartClass: 'mbsc-ic mbsc-ic-play2',
+        btnStopClass: 'mbsc-ic mbsc-ic-pause',
+        btnResetClass: 'mbsc-ic mbsc-ic-stop',
+        btnLapClass: 'mbsc-ic mbsc-ic-loop2'
     };
 
 })(jQuery);
