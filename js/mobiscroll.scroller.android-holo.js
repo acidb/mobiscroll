3c8709b9ec34fb0d82323fc7357bd8aeb61748b4
szily20 <szily20@gmail.com>
2014-05-05 12:05:59 +0300

    renamed android-ics to android-holo

--- js/mobiscroll.scroller.android-holo.js
+++ js/mobiscroll.scroller.android-holo.js
@@ -11,11 +11,20 @@
         useShortLabels: true,
         icon: { filled: 'star3', empty: 'star' },
         btnPlusClass: 'mbsc-ic mbsc-ic-arrow-down6',
-        btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up6'
+        btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up6',
+        onThemeLoad: function (lang, s) {
+            if (s.theme == 'android-ics') {
+                s.theme = 'android-holo';
+            } else if (s.theme == 'android-ics light') {
+                s.theme = 'android-holo light';
+            }
+        }
     };
 
     $.mobiscroll.themes['android-ics'] = theme;
     $.mobiscroll.themes['android-ics light'] = theme;
+    $.mobiscroll.themes['android-holo'] = theme;
+    $.mobiscroll.themes['android-holo light'] = theme;
 
 })(jQuery);
 
