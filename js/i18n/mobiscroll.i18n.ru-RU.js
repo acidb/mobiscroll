c673130578181ef3c09f0de3404915756cb5061a
Roman V. Nikolaev <rshadow@rambler.ru>
2014-06-05 23:16:04 +0400

    Russian translation

---
+++ js/i18n/mobiscroll.i18n.ru-RU.js
@@ -0,0 +1,56 @@
+(function ($) {
+    $.mobiscroll.i18n['ru-RU'] = $.extend($.mobiscroll.i18n['ru-RU'], {
+        // Core
+        setText:        'Установить',
+        cancelText:     'Отмена',
+        clearText:      'Очистить',
+        selectedText:   'Выбрать',
+        // Datetime component
+        dateFormat:     'dd.mm.yy',
+        dateOrder:      'ddmmyy',
+        dayNames:       ['воскресенье','понедельник','вторник','среда',
+                         'четверг','пятница','суббота'],
+        dayNamesShort:  ['вс','пн','вт','ср','чт','пт', 'сб'],
+        dayText:        'День',
+        hourText:       'Час',
+        minuteText:     'Минут',
+        monthNames:     ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль',
+                         'Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort:['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен',
+                         'Окт','Ноя','Дек'],
+        monthText:      'Месяц',
+        secText:        'Секунд',
+        timeFormat:     'HH:ii',
+        timeWheels:     'HHii',
+        yearText:       'Год',
+        nowText:        'Сейчас',
+        dateText:       'Дата',
+        timeText:       'Время',
+        calendarText:   'Календарь',
+        amText:         'До полудня',
+        pmText:         'После полудня',
+        // Calendar component
+        dateText:       'Дата',
+        timeText:       'Время',
+        calendarText:   'Календарь',
+        closeText:      'Закрыть',
+        // Daterange component
+        fromText:       'Начало',
+        toText:         'Конец',
+        // Measurement components
+        wholeText:      'Целое',
+        fractionText:   'Дробное',
+        unitText:       'Единица',
+        // Time / Timespan component
+        labels:         ['Лет','Месяцев','Дней',
+                         'Часов','Минут','Секунд',''],
+        labelsShort:    ['Лет','Мес','Дн','Час','Мин',
+                         'Сек',''],
+        // Timer component
+        startText:      'Старт',
+        stopText:       'Стоп',
+        resetText:      'Сбросить',
+        lapText:        'Круг',
+        hideText:       'Скрыть'
+    });
+})(jQuery);
