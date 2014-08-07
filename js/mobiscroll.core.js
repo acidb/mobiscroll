1213ff0e114bcc0d0e628bc8a3cfc6ac725446e9
Vladimir Kuleba <Vladimir.Kuleba@tonicforhealth.com>
2014-08-07 20:56:03 +0300

    Version 2.12.1 update

--- js/mobiscroll.core.js
+++ js/mobiscroll.core.js
@@ -1,5 +1,5 @@
 /*!
- * Mobiscroll v2.12.0
+ * Mobiscroll v2.12.1
  * http://mobiscroll.com
  *
  * Copyright 2010-2014, Acid Media
@@ -100,7 +100,7 @@
     };
 
     $.mobiscroll = $.mobiscroll || {
-        version: '2.12.0',
+        version: '2.12.1',
         util: {
             prefix: prefix,
             jsPrefix: pr,
