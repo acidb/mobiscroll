11305f97f76a26985a9a803370fcdc8fd93c4c28
Istvan Halmen <dioslaska@gmail.com>
2014-10-14 16:37:06 +0300

    ios -> ios-classic rename fixes

--- js/mobiscroll.widget.ios.js
+++ js/mobiscroll.widget.ios.js
@@ -1,28 +1,37 @@
 (function ($) {
 
-    $.mobiscroll.themes.ios = {
-        display: 'bottom',
-        dateOrder: 'MMdyy',
-        rows: 5,
-        height: 34,
-        minWidth: 55,
-        headerText: false,
-        showLabel: false,
-        btnWidth: false,
-        selectedLineHeight: true,
-        selectedLineBorder: 1,
-        useShortLabels: true,
-        deleteIcon: 'backspace3',
-        checkIcon: 'ion-ios7-checkmark-empty',
-        btnCalPrevClass: 'mbsc-ic mbsc-ic-arrow-left5',
-        btnCalNextClass: 'mbsc-ic mbsc-ic-arrow-right5',
-        btnPlusClass: 'mbsc-ic mbsc-ic-arrow-down5',
-        btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up5',
-        onThemeLoad: function (lang, s) {
-            if (s.theme) {
-                s.theme = s.theme.replace('ios7', 'ios');
+    var themes = $.mobiscroll.themes,
+        theme = {
+            display: 'bottom',
+            dateOrder: 'MMdyy',
+            rows: 5,
+            height: 34,
+            minWidth: 55,
+            headerText: false,
+            showLabel: false,
+            btnWidth: false,
+            selectedLineHeight: true,
+            selectedLineBorder: 1,
+            useShortLabels: true,
+            deleteIcon: 'backspace3',
+            checkIcon: 'ion-ios7-checkmark-empty',
+            btnCalPrevClass: 'mbsc-ic mbsc-ic-arrow-left5',
+            btnCalNextClass: 'mbsc-ic mbsc-ic-arrow-right5',
+            btnPlusClass: 'mbsc-ic mbsc-ic-arrow-down5',
+            btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up5',
+            // @deprecated since 2.14.0, backward compatibility code
+            // ---
+            onThemeLoad: function (lang, s) {
+                if (s.theme) {
+                    s.theme = s.theme.replace('ios7', 'ios');
+                }
             }
-        }
-    };
+            // ---
+        };
+
+    themes.ios = theme;
+
+    // @deprecated since 2.14.0, backward compatibility code
+    themes.ios7 = theme;
 
 })(jQuery);
