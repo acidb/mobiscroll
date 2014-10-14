3d84b629332e197032e030bf3ec9ecdb6bca42a7
szily20 <szily20@gmail.com>
2014-10-14 11:24:58 +0300

    ios theme renamed to ios-classic and ios7 theme renamed to ios

---
+++ js/mobiscroll.widget.ios-classic.js
@@ -0,0 +1,15 @@
+(function ($) {
+    $.mobiscroll.themes['ios-classic'] = {
+        display: 'bottom',
+        dateOrder: 'MMdyy',
+        rows: 5,
+        height: 30,
+        minWidth: 60,
+        headerText: false,
+        showLabel: false,
+        btnWidth: false,
+        selectedLineHeight: true,
+        selectedLineBorder: 2,
+        useShortLabels: true
+    };
+})(jQuery);
