5bc09cb63d6acb6915c3e38058d22ca95d8bfc1e
Istvan Halmen <dioslaska@gmail.com>
2014-11-14 17:46:57 +0200

    Android Holo Light and Windows Phone Light themes

--- js/mobiscroll.android-holo-light.js
+++ js/mobiscroll.android-holo-light.js
@@ -1,33 +1,33 @@
 (function ($) {
-    var themes = $.mobiscroll.themes,
-        theme = {
-            dateOrder: 'Mddyy',
-            //mode: 'mixed',
-            rows: 5,
-            minWidth: 76,
-            height: 36,
-            showLabel: false,
-            selectedLineHeight: true,
-            selectedLineBorder: 2,
-            useShortLabels: true,
-            icon: { filled: 'star3', empty: 'star' },
-            btnPlusClass: 'mbsc-ic mbsc-ic-arrow-down6',
-            btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up6',
-            onMarkupReady: function (markup, inst) {
-                markup.addClass('mbsc-android-holo');
-            }
-        };
+    
+    $.mobiscroll.themes['android-holo-light'] = {
+        dateOrder: 'Mddyy',
+        //mode: 'mixed',
+        rows: 5,
+        minWidth: 76,
+        height: 36,
+        showLabel: false,
+        selectedLineHeight: true,
+        selectedLineBorder: 2,
+        useShortLabels: true,
+        icon: { filled: 'star3', empty: 'star' },
+        btnPlusClass: 'mbsc-ic mbsc-ic-arrow-down6',
+        btnMinusClass: 'mbsc-ic mbsc-ic-arrow-up6',
+        onMarkupReady: function (markup) {
+            markup.addClass('mbsc-android-holo');
+        }
+    };
 
-    themes['mbsc-android-holo-light'] = theme;
-})(jQuery);
-
-(function ($) {
-    var themes = $.mobiscroll.themes.listview,
-        theme = {
+    $.mobiscroll.themes.listview['android-holo-light'] = {
         onInit: function () {
             $(this).closest('.mbsc-lv-cont').addClass('mbsc-lv-android-holo');
         }
+    };;
+
+    $.mobiscroll.themes.menustrip['android-holo-light'] = {
+        onMarkupReady: function (markup) {
+            markup.addClass('mbsc-android-holo');
+        }
     };
     
-    themes['mbsc-android-holo-light'] = theme;
 })(jQuery);
--- js/mobiscroll.wp-light.js
+++ js/mobiscroll.wp-light.js
@@ -1,80 +1,85 @@
 (function ($) {
+    
+    $.mobiscroll.themes['wp-light'] = {
+        minWidth: 76,
+        height: 76,
+        accent: 'none',
+        dateOrder: 'mmMMddDDyy',
+        headerText: false,
+        showLabel: false,
+        deleteIcon: 'backspace4',
+        icon: { filled: 'star3', empty: 'star' },
+        btnWidth: false,
+        btnStartClass: 'mbsc-ic mbsc-ic-play3',
+        btnStopClass: 'mbsc-ic mbsc-ic-pause2',
+        btnResetClass: 'mbsc-ic mbsc-ic-stop2',
+        btnLapClass: 'mbsc-ic mbsc-ic-loop2',
+        btnHideClass: 'mbsc-ic mbsc-ic-close',
+        btnCalPrevClass: 'mbsc-ic mbsc-ic-arrow-left2',
+        btnCalNextClass: 'mbsc-ic mbsc-ic-arrow-right2',
+        btnPlusClass: 'mbsc-ic mbsc-ic-plus',
+        btnMinusClass: 'mbsc-ic mbsc-ic-minus',
+        onMarkupInserted: function (elm) {
+            var click,
+                touch,
+                active;
 
-    var themes = $.mobiscroll.themes,
-        theme = {
-            minWidth: 76,
-            height: 76,
-            accent: 'none',
-            dateOrder: 'mmMMddDDyy',
-            headerText: false,
-            showLabel: false,
-            deleteIcon: 'backspace4',
-            icon: { filled: 'star3', empty: 'star' },
-            btnWidth: false,
-            btnStartClass: 'mbsc-ic mbsc-ic-play3',
-            btnStopClass: 'mbsc-ic mbsc-ic-pause2',
-            btnResetClass: 'mbsc-ic mbsc-ic-stop2',
-            btnLapClass: 'mbsc-ic mbsc-ic-loop2',
-            btnHideClass: 'mbsc-ic mbsc-ic-close',
-            btnCalPrevClass: 'mbsc-ic mbsc-ic-arrow-left2',
-            btnCalNextClass: 'mbsc-ic mbsc-ic-arrow-right2',
-            btnPlusClass: 'mbsc-ic mbsc-ic-plus',
-            btnMinusClass: 'mbsc-ic mbsc-ic-minus',
-            onMarkupInserted: function (elm, inst) {
-                var click,
-                    touch,
-                    active,
-                    s = inst.settings;
+            elm.addClass('mbsc-wp');
 
-                elm.addClass('mbsc-wp');
-
-                $('.dwb-s .dwb', elm).addClass('mbsc-ic mbsc-ic-checkmark');
-                $('.dwb-c .dwb', elm).addClass('mbsc-ic mbsc-ic-close');
-                $('.dwb-cl .dwb', elm).addClass('mbsc-ic mbsc-ic-close');
-                $('.dwb-n .dwb', elm).addClass('mbsc-ic mbsc-ic-loop2');
-
-                $('.dwwl', elm).on('touchstart mousedown DOMMouseScroll mousewheel', function (e) {
-                    if (e.type === 'mousedown' && touch) {
-                        return;
-                    }
-                    touch = e.type === 'touchstart';
-                    click = true;
-                    active = $(this).hasClass('wpa');
-                    $('.dwwl', elm).removeClass('wpa');
-                    $(this).addClass('wpa');
-                }).on('touchmove mousemove', function () {
-                    click = false;
-                }).on('touchend mouseup', function (e) {
-                    if (click && active && $(e.target).closest('.dw-li').hasClass('dw-sel')) {
-                        $(this).removeClass('wpa');
-                    }
-                    if (e.type === 'mouseup') {
-                        touch = false;
-                    }
-                    click = false;
-                });
-            },
-            onThemeLoad: function (lang, s) {
-                if (lang && lang.dateOrder && !s.dateOrder) {
-                    var ord = lang.dateOrder;
-                    ord = ord.match(/mm/i) ? ord.replace(/mmMM|mm|MM/,  'mmMM') : ord.replace(/mM|m|M/,  'mM');
-                    ord = ord.match(/dd/i) ? ord.replace(/ddDD|dd|DD/,  'ddDD') : ord.replace(/dD|d|D/,  'dD');
-                    s.dateOrder = ord;
+            $('.dwwl', elm).on('touchstart mousedown DOMMouseScroll mousewheel', function (e) {
+                if (e.type === 'mousedown' && touch) {
+                    return;
                 }
+                touch = e.type === 'touchstart';
+                click = true;
+                active = $(this).hasClass('wpa');
+                $('.dwwl', elm).removeClass('wpa');
+                $(this).addClass('wpa');
+            }).on('touchmove mousemove', function () {
+                click = false;
+            }).on('touchend mouseup', function (e) {
+                if (click && active && $(e.target).closest('.dw-li').hasClass('dw-sel')) {
+                    $(this).removeClass('wpa');
+                }
+                if (e.type === 'mouseup') {
+                    touch = false;
+                }
+                click = false;
+            });
+        },
+        onThemeLoad: function (lang, s) {
+            if (lang && lang.dateOrder && !s.dateOrder) {
+                var ord = lang.dateOrder;
+                ord = ord.match(/mm/i) ? ord.replace(/mmMM|mm|MM/,  'mmMM') : ord.replace(/mM|m|M/,  'mM');
+                ord = ord.match(/dd/i) ? ord.replace(/ddDD|dd|DD/,  'ddDD') : ord.replace(/dD|d|D/,  'dD');
+                s.dateOrder = ord;
             }
-        };
+        },
+        onInit: function (inst) {
+            var buttons = inst.buttons;
 
-    themes['mbsc-wp-light'] = theme;
-})(jQuery);
+            buttons.set.icon = 'checkmark';
+            buttons.cancel.icon = 'close';
+            buttons.clear.icon = 'close';
 
-(function ($) {
-    var theme = {
-        onInit: function () {
-             $(this).closest('.mbsc-lv-cont').addClass('mbsc-lv-wp');
+            if (buttons.now) {
+                buttons.now.icon = 'loop2';
+            }
         }
     };
     
-    $.mobiscroll.themes.listview['mbsc-wp-light'] = theme;
+    $.mobiscroll.themes.listview['wp-light'] = {
+        onInit: function () {
+            $(this).closest('.mbsc-lv-cont').addClass('mbsc-lv-wp');
+        }
+    };
+    
+    $.mobiscroll.themes.menustrip['wp-light'] = {
+        onMarkupReady: function (markup) {
+            markup.addClass('mbsc-wp');
+        }
+    };
+    
 })(jQuery);
 
 
