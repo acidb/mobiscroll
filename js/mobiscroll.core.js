3c520169e345c1fceb2741ad43e8f0e3ae107612
Istvan Halmen <dioslaska@gmail.com>
2014-12-17 12:43:51 +0200

    2.14.3 Version numbers

--- js/mobiscroll.core.js
+++ js/mobiscroll.core.js
@@ -1,5 +1,5 @@
 /*!
- * Mobiscroll v2.14.2
+ * Mobiscroll v2.14.3
  * http://mobiscroll.com
  *
  * Copyright 2010-2014, Acid Media
@@ -80,7 +80,7 @@
     };
 
     $.mobiscroll = $.mobiscroll || {
-        version: '2.14.2',
+        version: '2.14.3',
         util: {
             prefix: prefix,
             jsPrefix: pr,
