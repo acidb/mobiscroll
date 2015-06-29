19a18daadce44740a49c3b4df6769347a02e43c3
MRokas <rokasmuningis@gmail.com>
2015-06-29 19:25:27 +0300

    Created mobiscroll.i18n.lt.js
    Internationalization file for Lithuanian language.

---
+++ js/i18n/mobiscroll.i18n.lt.js
@@ -0,0 +1,53 @@
+(function ($) {
+    $.mobiscroll.i18n['lt'] =  {
+        // Core
+        setText: 'OK',
+        cancelText: 'Atšaukti',
+        clearText: 'Išvalyti',
+        selectedText: 'Pasirinktas',
+        // Datetime component
+        dateFormat: 'yy.mm.dd',
+        dateOrder: 'yymmdd',
+        dayNames: ['Sekmadienis', 'Pirmadienis', 'Antradienis', 'Trečiadienis', 'Ketvirtadienis', 'Penktadienis', 'Šeštadienis'],
+        dayNamesShort: ['S', 'Pr', 'A', 'T', 'K', 'Pn', 'Š'],
+		dayNamesMin: ['S', 'Pr', 'A', 'T', 'K', 'Pn', 'Š'],	// same as dayNamesShort, included just incase
+        dayText: 'Diena',
+        hourText: 'Valanda',
+        minuteText: 'Minutes',
+        monthNames: ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'],
+        monthNamesShort: ['Sau', 'Vas', 'Kov', 'Bal', 'Geg', 'Bir', 'Lie', 'Rugp', 'Rugs', 'Spa', 'Lap', 'Gruo'], // Literally never used
+        monthText: 'Mėnuo',
+        secText: 'Sekundes',
+        amText: 'am', 
+        pmText: 'pm',
+        timeFormat: 'hh:ii',
+        timeWheels: 'hhii',
+        yearText: 'Metai',
+        nowText: 'Dabar',
+        // Calendar component
+        firstDay: 1,
+        dateText: 'Data',
+        timeText: 'Laikas',
+        calendarText: 'Kalendorius',
+        closeText: 'Uždaryti',
+        // Daterange component
+        fromText: 'Nuo',
+        toText: 'Iki',
+        // Measurement components
+        wholeText: 'Visas',
+        fractionText: 'Frakcija',
+        unitText: 'Vienetas',
+        // Time / Timespan component
+        labels: ['Metai', 'Mėnesiai', 'Dienos', 'Valandos', 'Minutes', 'Sekundes', ''],
+        labelsShort: ['m', 'mėn.', 'd', 'h', 'min', 's', ''],
+        // Timer component
+        startText: 'Pradėiti',
+        stopText: 'Sustoti',
+        resetText: 'Išnaujo',
+        lapText: 'Ratas',
+        hideText: 'Slėpti',
+        // Listview
+        backText: 'Atgal',
+        undoText: 'Atšaukti veiksmą'
+    });
+})(jQuery);
