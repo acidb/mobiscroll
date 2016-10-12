8ba4c4e8d55d1a29812bc49318808a0ed4bd0afe
Istvan Halmen <dioslaska@gmail.com>
2016-10-12 09:13:25 +0300

    Version numbers

--- js/mobiscroll.core.js
+++ js/mobiscroll.core.js
@@ -1,5 +1,5 @@
 /*!
- * Mobiscroll v3.0.0-beta5
+ * Mobiscroll v3.0.0-beta6
  * http://mobiscroll.com
  *
  * Copyright 2010-2016, Acid Media
@@ -101,7 +101,7 @@ var mobiscroll = mobiscroll || {};
 
     ms = mobiscroll = {
         $: $,
-        version: '3.0.0-beta5',
+        version: '3.0.0-beta6',
         util: {
             prefix: prefix,
             jsPrefix: pr,
